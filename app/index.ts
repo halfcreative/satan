957f77ba738e398f227a9b157dc84c04a727c83a
Alan Sato <sam.alan.sato@gmail.com>
2020-08-23 19:23:47 -0500

    sma added

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,4 @@
 import { Handler, Context, Callback } from "aws-lambda";
-import { AssetService } from "./services/AssetService";
 import { EventHandler } from "./workers/EventHandler";
 
 let eventHandler: EventHandler | null = null;
--- app/utilities/TAUtils.ts
+++ app/utilities/TAUtils.ts
@@ -1,4 +1,21 @@
 export class TAUtils {
 
+    /**
+     * Get the simple moving average from a list of numbers.
+     * @param values Array of numbers ordered from most recent to least recent.
+     * @param range The range to calculate the simple moving average 
+     * @returns one SMA value for the most recent data point provided.
+     */
+    public sma(values: Array<number>, range: number): number {
+        const array = values.slice(0, range)
+        const sum = array.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+        return (sum / range);
+    }
+
+    public ema() {
+
+    }
 
 }
