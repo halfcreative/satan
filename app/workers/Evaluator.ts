4723f1add77dae014dacf677218b669e71f728f7
Alan Sato <sam.alan.sato@gmail.com>
2020-10-14 23:40:40 -0500

    calculate indicators in a seperate function

--- app/workers/Evaluator.ts
+++ app/workers/Evaluator.ts
@@ -15,31 +15,34 @@ export class Evaluator {
     }
 
     public evaluateAssetAndStoreEvaluation(currency: string, assetInfo: AssetInformationModel): Promise<Evaluation> {
-        const evaluation = this.generateFullEvaluation(assetInfo)
+        const evaluation = this.generateFullEvaluation(assetInfo);
         return this.storeEvaluation(currency, evaluation);
     }
 
     private generateFullEvaluation(info: AssetInformationModel): Evaluation {
-        console.log("Asset Info Model", info.lastEval);
+        console.info("Generating Evaluation");
         const evaluation = new Evaluation();
         evaluation.price = parseFloat(info.ticker.price);
-        const closingHistory = this.assetService.singleSetHistory(info.history, 4) // get only the history of closing values
-        // const macd = new MACD();
-        evaluation.indicators = new Indicators();
+        evaluation.indicators = this.calculateIndicators(info);
+        return evaluation;
+    }
 
-        evaluation.indicators.sma50 = TA.sma(closingHistory, 50);
-        evaluation.indicators.sma20 = TA.sma(closingHistory, 20);
-        evaluation.indicators.ema12 = TA.ema(closingHistory, 12)[0];
-        evaluation.indicators.ema26 = TA.ema(closingHistory, 26)[0];
-        evaluation.indicators.rsi14 = TA.rsi(closingHistory, 14);
+    private calculateIndicators(assetInfo: AssetInformationModel): Indicators {
+        console.info("Calculating Indicators");
+        const indicators = new Indicators();
+        const closingHistory = this.assetService.singleSetHistory(assetInfo.history, 4); // get only the history of closing values
+
+        indicators.sma50 = TA.sma(closingHistory, 50);
+        indicators.sma20 = TA.sma(closingHistory, 20);
+        indicators.ema12 = TA.ema(closingHistory, 12)[0];
+        indicators.ema26 = TA.ema(closingHistory, 26)[0];
+        indicators.rsi14 = TA.rsi(closingHistory, 14);
 
         const macd = TA.macd(closingHistory, 20);
         const macdSignal = TA.macdSignal(macd);
+        indicators.macd = assetInfo.lastEval ? new MACD(macd[0], macdSignal[0], assetInfo.lastEval.indicators.macd) : new MACD(macd[0], macdSignal[0]);
 
-
-        evaluation.indicators.macd = info.lastEval ? new MACD(macd[0], macdSignal[0], info.lastEval.indicators.macd) : new MACD(macd[0], macdSignal[0]);
-
-        return evaluation;
+        return indicators;
     }
 
     private storeEvaluation(currency: string, evaluation: Evaluation): Promise<Evaluation> {
